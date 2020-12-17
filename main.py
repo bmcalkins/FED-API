cb0e3ec737e3dc8e8a642045f1359756dd34a169
Bmcalkins <b.raders1013@gmail.com>
2020-12-16 17:42:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from flask import Flask 
+from flask_restful import Api, Resource
+from fredapi import Fred
+
+app = Flask(__name__)
+api = Api(app)
+fred = Fred(api_key='6cd659eeb72f5cda524e78dbb3e4d577')
+
+@app.route('/')
+def index():
+    return "Hello World"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
